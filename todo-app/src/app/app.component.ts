46f95288b775690f6bb557d839a47ea595852e30
sylhare <sylhare@outlook.com>
2018-10-04 13:12:51 -0400

    Add todo

--- todo-app/src/app/app.component.ts
+++ todo-app/src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo-app';
+  todoArray=[]
+    
+  addTodo(value){
+    this.todoArray.push(value)
+    console.log(this.todo)
+  }
 }
