fe7ebbb124a23717d963c28f724d6a19e11a151e
sylhare <sylhare@outlook.com>
2018-10-12 13:42:13 -0400

    Reformat

--- todo-app/src/app/app.component.ts
+++ todo-app/src/app/app.component.ts
@@ -1,30 +1,29 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo-app';
-  todoArray=[]
-    
-  addTodo(value){
-    this.todoArray.push(value)
-    console.log(this.todoArray)  
-  }
-    
-  deleteItem(todo){
-    this.todoArray = this.todoArray.filter(value => value !== todo);  
-  }  
-    
-  todoSubmit(value:any){
-     if(value!==""){
-    this.todoArray.push(value.todo)
-     //this.todoForm.reset()
-    }else{
-      alert('Field required **')
+    title = 'todo-app';
+    todoArray = []
+
+    addTodo(value) {
+        this.todoArray.push(value)
+        console.log(this.todoArray)
     }
-    
-  }
+
+    deleteItem(todo) {
+        this.todoArray = this.todoArray.filter(value => value !== todo);
+    }
+
+    todoSubmit(value: any) {
+        if (value !== "") {
+            this.todoArray.push(value.todo)
+        } else {
+            alert('Field required **')
+        }
+
+    }
 }
